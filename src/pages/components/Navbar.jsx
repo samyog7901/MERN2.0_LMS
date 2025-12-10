3182a54cb71ba6339a4a1a8eb297afe36fdc0732
samyog7901 <samyogkhadka247@gmail.com>
2025-12-10 22:35:02 +0545

    UI Improvementss

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -87,7 +87,7 @@ const Navbar = () => {
 
             {/* Cart */}
             <button className="relative hover:text-blue-600 
-              dark:hover:text-blue-400 transition">
+              dark:hover:text-blue-400 transition-colors duration-300 dark:text-white">
               <svg xmlns="http://www.w3.org/2000/svg" 
                 className="w-6 h-6" fill="none" viewBox="0 0 24 24"
                 strokeWidth="2" stroke="currentColor">
@@ -102,7 +102,7 @@ const Navbar = () => {
             </button>
 
             {/* User */}
-            <button className="hover:text-blue-600 dark:hover:text-blue-400 transition">
+            <button className="hover:text-blue-600 dark:hover:text-blue-400 transition-colors duration-300 dark:text-white">
               <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6"
                 fill="none" viewBox="0 0 24 24" strokeWidth="2"
                 stroke="currentColor">
