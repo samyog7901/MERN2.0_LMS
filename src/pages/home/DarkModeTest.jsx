95a3012c613c9e5aa30b42329868a7f62a96d69e
samyog7901 <samyogkhadka247@gmail.com>
2025-12-10 16:52:54 +0545

    UI Improvementss

--- src/pages/home/DarkModeTest.jsx
+++ src/pages/home/DarkModeTest.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const DarkModeTest = () => {
   return (
-    <div>DarkModeTest</div>
-  )
-}
+    <div className="bg-white dark:bg-gray-900 text-black dark:text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Dark Mode Test</h1>
+    </div>
+  );
+};
 
-export default DarkModeTest
+export default DarkModeTest;
