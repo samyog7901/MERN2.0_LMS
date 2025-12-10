375f50f18c9fd5a253338d668e4374ccd1dff89a
samyog7901 <samyogkhadka247@gmail.com>
2025-12-10 16:46:39 +0545

    UI Improvementss

--- src/App.jsx
+++ src/App.jsx
@@ -17,4 +17,11 @@ const App = () => {
   )
 }
 
-export default App
+export default  function App()
+{
+  return (
+    <div className="bg-white dark:bg-gray-900 text-black dark:text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Dark Mode Test</h1>
+    </div>
+  );
+}
